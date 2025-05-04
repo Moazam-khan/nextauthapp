a8575589ff25ea6d660da1c8e2af0cf708134383
Moazam-khan <moazamkhan8999@gmail.com>
2025-05-04 23:49:45 +0500

    forgetpassword

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${robotoMono.variable} antialiased`}
       >
+      
         {children}
       </body>
     </html>
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
 
-  const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail'
+  const isPublicPath = path === '/' || path === '/login' || path === '/signup' || path === '/verifyemail'
 
   const token = request.cookies.get('token')?.value || ''
 
@@ -14,7 +14,7 @@ export function middleware(request: NextRequest) {
   }
 
   if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL('/login', request.nextUrl))
+    return NextResponse.redirect(new URL('/', request.nextUrl))
   }
     
 }
